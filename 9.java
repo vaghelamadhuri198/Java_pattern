dffd0a7864a905492b0e5a24df5076b85fc95381
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 15:04:39 +0530

    9.java

---
+++ 9.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		int n = 5 ;
+
+		//outer loop
+		for (int i = 1; i <= n; i++) {
+			//spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			// inner loop
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

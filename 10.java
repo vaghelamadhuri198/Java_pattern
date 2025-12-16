e721678534312b28775f2b7a2eecc88961eab424
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 15:05:47 +0530

    10.java

---
+++ 10.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		int n = 5 ;
+
+
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if ((i + j) % 2 == 0) {
+					System.out.print(0);
+				} else {
+					System.out.print(1);
+				}
+
+			}
+
+			System.out.println();
+		}
+	}
+}

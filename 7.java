f83bb868b947de971e8e96e9dbef35b87ad86683
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 15:01:33 +0530

    7.java

---
+++ 7.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int n = 5 ;
+
+
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= n - i; j++) {
+				System.out.print(j + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}

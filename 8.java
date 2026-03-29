b9da75755b67f5a2d791606718b97d41763e0dab
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-03-29 13:15:56 +0530

    8.java
    👉 i = 0
    Inner loop: j = 0 → 0 (1 time)
    number = 1 → print 1
    Output :
    1
    
    👉 i = 1
    Inner loop: j = 0 → 1 (2 times)
    number = 2 → print 2
    number = 3 → print 3
    Output:
    2 3
    
    👉 i = 2
    Inner loop: j = 0 → 2 (3 times)
    number = 4 → print 4
    number = 5 → print 5
    number = 6 → print 6
    Output:
    4 5 6
    
    👉 i = 3
    Inner loop: j = 0 → 3 (4 times)
    number = 7 → print 7
    number = 8 → print 8
    number = 9 → print 9
    number = 10 → print 10
    Output:
    7 8 9 10
    
    👉 i = 4
    Inner loop: j = 0 → 4 (5 times)
    number = 11 → print 11
    number = 12 → print 12
    number = 13 → print 13
    number = 14 → print 14
    number = 15 → print 15
    Output:
    11 12 13 14 15
    
    👉 i = 5
    Inner loop: j = 0 → 5 (6 times)
    number = 16 → print 16
    number = 17 → print 17
    number = 18 → print 18
    number = 19 → print 19
    number = 20 → print 20
    number = 21 → print 21
    Output:
    16 17 18 19 20 21

---
+++ 8.java
@@ -0,0 +1,16 @@
+public class Main {
+public static void main(String[] args) {
+int n = 5 ;
+int number = 0;
+
+for (int i = 0; i <= n; i++) {  
+                    for (int j = 0; j <=i; j++) {  
+                            number ++;  
+                            System.out.print(number + " ");  
+                    }  
+
+                    System.out.println();  
+            }  
+    }
+
+}
